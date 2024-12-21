44430e7bb1c3e37a54f402e4aed22a0dc1e10aa5
UssopSniperKing <39530006+UssopSniperKing@users.noreply.github.com>
2024-12-21 23:45:09 +0100

    Added Transformations class

--- src/bumblebee_kinematic_model.py
+++ src/bumblebee_kinematic_model.py
@@ -1,14 +1,27 @@
+from src.core import Scalar
+import numpy as np
 
-def bumblebee_kinematics_model(PHI=115.0, phi_m=24.0, dTau=0.00, alpha_down=70.0, alpha_up=-40.0, tau=0.22, theta=12.55/2):
+def bumblebee_kinematics_model(
+    number_time_steps: int,
+    PHI: Scalar = 115.0,
+    phi_m: Scalar = 24.0,
+    dTau: Scalar = 0.00,
+    alpha_down: Scalar = 70.0,
+    alpha_up: Scalar = -40.0,
+    tau: Scalar = 0.22,
+    theta: Scalar = 12.55 / 2,
+) -> tuple: # todo : add tests
     """
     Kinematics model for a bumblebee bombus terrestris [Engels et al PRL 2016, PRF 2019]
 
     Note motion starts with downstroke. Defaults are set to values used in [Engels et al PRL 2016, PRF 2019].
-    
+
     Alpha is piecewise constant with sin transition, theta is constant and phi is sinusoidal.
 
     Parameters
     ----------
+    number_time_steps : int
+        Number of time steps
     PHI : float, scalar
         Stroke amplitude (deg)
     phi_m : float, scalar
@@ -19,15 +32,17 @@ def bumblebee_kinematics_model(PHI=115.0, phi_m=24.0, dTau=0.00, alpha_down=70.0
         Featherng angle_rad during downstroke. (deg)
     alpha_up : float, scalar, optional
         Feathering angle_rad during upstroke. (deg)
-    tau : 
+    tau :
         duration of wing rotation
-    theta : 
+    theta :
         constant deviation angle_rad (deg)
-    
+
 
     Returns
     -------
-    alpha, phi, theta
+    time, alpha, phi, theta
     """
 
-    pass # todo : need to be adapted to work with Vector3D and Angle classes
+    time = np.linspace(0, 1, number_time_steps)
+
+    pass  # todo : need to be adapted to work with Vector3D and Angle classes 
--- src/core/referentials.py
+++ src/core/referentials.py
@@ -1,8 +1,27 @@
 from enum import Enum, auto
-
+from .transform_func import stroke_to_body_matrix, body_to_wing_matrix, wing_to_global_matrix
 
 class Referential(Enum):
     GLOBAL = auto()
     WING = auto()
     BODY = auto()
     STROKE = auto()
+
+
+class Transformations(): # todo : add tests
+ 
+    _transformations = {}
+    _is_initialized = False
+
+    @staticmethod
+    def initialize(): # todo : add angles as arguments, perform checks on the angles
+        Transformations._transformations = {
+            (Referential.STROKE, Referential.BODY): stroke_to_body_matrix(),
+            (Referential.BODY, Referential.WING): body_to_wing_matrix(),
+            (Referential.WING, Referential.GLOBAL): wing_to_global_matrix()
+        }
+        Transformations._is_initialized = True
+
+    @staticmethod
+    def get_matrix(source: Referential, target: Referential): # todo : build something more robust
+        return Transformations._transformations[(source, target)]
