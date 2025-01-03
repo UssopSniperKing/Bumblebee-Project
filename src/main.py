23f41503db5832598b8750cc1c4830fccd274268
Jbestre <you@example.com>
2025-01-03 15:17:51 +0100

    main

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from data import KinematicsSolutionHolder
-from kinematics_evaluations import evaluate_angles_kinematics, evaluate_velocities
+from kinematics_evaluations import evaluate_angles_kinematics, define_unit_vectors, evaluate_angular_velocity, evaluate_tip_velocity
 from initialize_transformations import initialize_transformations
 from plot_kinematics import plot_kinematics
 
@@ -12,9 +12,16 @@ def main() -> None:
     NUMBER_TIME_STEPS = 1000
     Kinematics = KinematicsSolutionHolder()
     Kinematics = evaluate_angles_kinematics(NUMBER_TIME_STEPS, Kinematics)
-    Kinematics = evaluate_velocities(Kinematics)
+
+    Kinematics = define_unit_vectors(Kinematics)
+
+    Kinematics = evaluate_angular_velocity(Kinematics)
+    
+    Kinematics = evaluate_tip_velocity(Kinematics)
     initialize_transformations(Kinematics)
+
     plot_kinematics(Kinematics)
+
 
 
 if __name__ == '__main__':
